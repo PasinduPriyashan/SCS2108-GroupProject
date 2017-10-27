974eee05e483612af7fb28c5634db834cbbf7cc4
Kavinda Gunasekera <ksgunasekera@gmail.com>
2017-10-27 14:56:07 +0530

    Add files via upload
    new home controller

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,15 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: X556U- DM818
- * Date: 10/27/2017
- * Time: 2:27 PM
- */
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller
-{
-    public function index()
-    {
-        $this->load->view('home');
-    }
+class Home extends CI_Controller {
+
+	
+	public function index()
+	{
+
+		 if($this->session->userdata('logged_in')){
+		 	$session_data = $this->session->userdata('logged_in');
+		 	$data['id'] = $session_data['id'];
+		 	$data['email'] = $session_data['email'];
+		 	$data['username'] = $session_data['username'];
+		 	$data['usertype'] = $session_data['usertype'];
+		 	
+		 		$this->load->view('admin',$data);
+		 	
+		 	
+		 }else{
+		 	$this->load->view('home');
+		 }
+	}
+	public function logout(){
+		$this->session->unset_userdata('logged_in');
+		$this->session->sess_destroy();
+		redirect(site_url('home'),'refresh');
+	}
+
+	
 }
+ 
