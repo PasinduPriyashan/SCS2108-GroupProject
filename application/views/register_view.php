2faa017dd6cddcd64d8abe78a62b8320ab41be36
PasinduPriyashan <pasindu95.m@gmail.com>
2017-10-27 15:38:32 +0530

    routes 6

--- application/views/register_view.php
+++ application/views/register_view.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?php echo base_url('asserts/css/bootstrap.min.css');?>" >
+      <link href="<?php echo base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
    <div class="container" style="margin-top:80px;">
