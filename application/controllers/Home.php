2367c4a15a50131b3026c3062d4e9c1519dcdecc
PasinduPriyashan <pasindu95.m@gmail.com>
2017-10-27 14:30:15 +0530

    Home added as default controller

---
+++ application/controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: X556U- DM818
+ * Date: 10/27/2017
+ * Time: 2:27 PM
+ */
+
+class Home extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('home');
+    }
+}
