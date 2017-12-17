5f6aa4d4b7f572d05456330caef7f274a93be1fa
Pasindu Weerasinghe <pasindurohana@gmail.com>
2017-12-17 14:24:02 +0530

    push admin and home models

--- samikta/application/models/Home_model.php
+++ samikta/application/models/Home_model.php
@@ -5,7 +5,29 @@
 class Home_model extends CI_Model
 {
 
-  
+  public function check_log(){
+    //$this->db->from('user_login');
+    $this->db->where('user_name', $this->input->post('uname',TRUE));
+    $this->db->where('user_password', $this->input->post('password',TRUE));
+    $query = $this->db->get('user_login');
+    $row = $query->row();
+    if($query->num_rows()==1){
+      return $row;
+    }else {
+      return false;
+    }
+  }
+
+  public function proj_view($id){
+    $this->db->where('projid', $id);
+    $query = $this->db->get('project');
+    $row = $query->row();
+    if($query->num_rows()==1){
+      return $row;
+    }else {
+      return false;
+    }
+  }
 
 
 }
