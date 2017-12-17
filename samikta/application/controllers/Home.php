e075d9c76546dac9fbc6ac15df3f0155a1e4e93d
Pasindu Weerasinghe <pasindurohana@gmail.com>
2017-12-17 14:22:22 +0530

    Admin and Home controller push

--- samikta/application/controllers/Home.php
+++ samikta/application/controllers/Home.php
@@ -29,10 +29,62 @@ class Home extends CI_Controller
   public function register(){
     $this->load->view('register');
   }
+  public function project_view(){
+    $this->load->view('project_view');
+  }
 
-  
+  public function user_log(){
+    $this->load->model('Home_model');
+    $row = $this->Home_model->check_log();
+    if ($row!=false) {
+      echo $row->fname;
+      $user_data = array(
+        'username' =>$row->user_name ,
+        'fname' =>$row->fname ,
+        'lname' =>$row->lname ,
+        'email' =>$row->user_rmail,
+        'loggedin'=>TRUE);
 
-  
+        $this->session->set_userdata($user_data);
+        $this->session->set_flashdata('welcome','Welcome!');
+        redirect('Admin/index');
+
+
+    }else {
+      $this->load->view('login');
+    }
+
+  }
+
+  public function user_logout(){
+    $this->session->unset_userdata('fname');
+    $this->session->unset_userdata('lname');
+    $this->session->unset_userdata('email');
+    $this->session->unset_userdata('username');
+    $this->session->unset_userdata('loggedin');
+
+
+    redirect('Home/login');
+  }
+
+  public function proj_view($id){
+    $this->load->model('Home_model');
+    $row = $this->Home_model->proj_view($id);
+    if ($row!=false) {
+      $data = array('projid' => $row->projid,
+      'projtitle' => $row->projtitle,
+      'projdes' => $row->projdes,
+      'image' => $row->image,
+      'date' => $row->date
+     );
+      $this->load->view('project_view',$data);
+
+    }
+
+    else {
+      echo "fail";
+    }
+  }
 }
 
  ?>
