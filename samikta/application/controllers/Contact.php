71c386f459f4346184c253e86f511b766f5b6e93
Pasindu Weerasinghe <pasindurohana@gmail.com>
2017-12-19 08:10:16 +0200

    contact controller

--- samikta/application/controllers/Contact.php
+++ samikta/application/controllers/Contact.php
@@ -1,39 +1,23 @@
 <?php
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-Class Contact extends CI_Controller{
-	public function index(){
+/**
+ *
+ */
+class Contact extends CI_Controller
+{
 
-		$this->load->view('contact');
-	}
-	public function insert(){
-		//echo "Hello";
-		$this->load->library('form_validation');
-		$this->form_validation->set_rules('firstName','First Name','required');
-		$this->form_validation->set_rules('lastName','Last Name','required');
-		$this->form_validation->set_rules('email','Email','required|valid_email');
-		$this->form_validation->set_rules('message','Message','required');
+  public function insertdata(){
+    $this->load->model('Contact_model');
+    $row = $this->Contact_model->insdata();
 
+    if($row!=false){
+      $this->session->set_flashdata('contact', 'Your message has been sent!');
+      redirect('Home/contact');
+    }else {
+      echo "fail";
+    }
 
-
-		if ($this->form_validation->run()) {
-
-			$this->load->model('User');
-			$contactInfo=array(
-				"firstName"=>$this->input->post("firstName"),
-				"lastName"=>$this->input->post("lastName"),
-				"email"=>$this->input->post("email"),
-				"subject"=>$this->input->post("subject"),
-				"website"=>$this->input->post("website"),
-				"message"=>$this->input->post("message")
-
-			);
-			$this->User->contactUs($contactInfo);
-			redirect(site_url('Contact/index'));
-		}else{
-			$this->index();
-		}
-	}
-	
-	
+  }
 }
+
+
 ?>
